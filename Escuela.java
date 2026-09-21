b1bdee0ae69beb859d02cc5989046a29f06b6484
yoanaprogutn-lang <yoanaprog.utn@gmail.com>
2026-09-21 00:44:35 -0300

    agregando a la clase Escuela.java, atributos y contructor

---
+++ Escuela.java
@@ -0,0 +1,10 @@
+public class Escuela {
+    private String nombre;
+    private String playa;
+
+    public Escuela(String nombre, string playa){
+    this.nombre = nombre;
+    this.playa = playa;
+    }
+}
+  
