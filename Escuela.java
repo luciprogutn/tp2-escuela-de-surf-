4ebfb4573a79db8731cff7dc63e1f4d3ee6613cb
yoanaprogutn-lang <yoanaprog.utn@gmail.com>
2026-09-21 00:55:17 -0300

    añadiendo a escuela.java punto C.7, metodo de descripcion

--- Escuela.java
+++ Escuela.java
@@ -6,5 +6,24 @@ public class Escuela {
     this.nombre = nombre;
     this.playa = playa;
     }
+
+    public String getNombre() {
+    return nombre;
+    }
+    public String getPlaya() {
+    return playa;
+    }
+    //Setters (por si cambia el nombre de la sede de la clase)
+    public void setNombre(String nombre) {
+    this.nombre = nombre;
+    }
+
+    public void setPlaya(String playa) {
+    this.playa = playa;
+    }
+
+    // este es le punto 7 que dice que tenemos que retornar una descripcion.
+    public String descripcion() {
+    return "Escuela: " + nombre + " (Playa: " + playa + ")";
+    }
 }
-  
