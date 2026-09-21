5b0d735894f2b14e4146d0190d9b863a7ac2e75f
luciprogutn <luciprog.utn@gmail.com>
2026-09-21 18:25:05 -0300

    agregando funcion disponible o en reparacion, en tabla.java

--- Tabla.java
+++ Tabla.java
@@ -4,15 +4,24 @@ public class Tabla {
     private boolean enReparacion;
 
 
-    public Tabla(int codigo, double largoPies, boolean enReparacion){
+    public Tabla(int codigo, double largoPies, boolean enReparacion) {
         this.codigo = codigo;
-        this.largoPies= largoPies;
+        this.largoPies = largoPies;
         this.enReparacion = enReparacion;
 
     }
+
+    public void LargoPies(double largoPies) {
+        if (largoPies >= 5 && largoPies <= 10) {
+            this.largoPies = largoPies;
+        } else System.out.println("la Tabla no esta dentro del rango");
+
+    }
+
     public Tabla(int codigo, double largoPies) {
         this(codigo, largoPies, false);
     }
+
     public boolean disponible() {
         return !this.enReparacion;
     }
@@ -30,13 +39,17 @@ public class Tabla {
 
         return largoPies;
     }
+
     public void setEnReparacion(boolean enReparacion) {
 
         this.enReparacion = enReparacion;
     }
 
-    public String descripcion() {
-        return "Tabla: " + codigo + " " + largoPies + " ";
+    public boolean Disponible() {
+        return !enReparacion;
     }
 
+    public String descripcion() {
+        return "Tabla: [codigo:" + codigo + " | largo: " + largoPies + " pies | Estado:" + enReparacion + "]";
+    }
 }
